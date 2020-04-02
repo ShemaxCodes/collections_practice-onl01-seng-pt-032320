50ef0f53721380497951f1e57c3c167b3b87ef98
Jashema Panapa <jcpanapa@gmail.com>
2020-04-02 02:04:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -38,7 +38,7 @@ def sum_array(array)
   array.inject { |answer, element| answer + element }
 end
 
-def add_s
+def add_s(array)
   array.each_with_index.collect do |element, index| 
   if index == 1 
     return element
